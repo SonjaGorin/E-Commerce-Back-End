2857a4d8b28a0049415e8fd5bcd9dcc4f5421bab
Sonja <sonjaljatkovic@gmail.com>
2023-11-08 12:00:16 -0500

    started models/index.js

--- models/index.js
+++ models/index.js
@@ -4,13 +4,13 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
+Product.belongsTo(Category);
 
-// Categories have many Products
+Category.hasMany(Product);
 
-// Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag);
 
-// Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product);
 
 module.exports = {
   Product,
