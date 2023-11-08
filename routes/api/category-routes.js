1c9e4f7c0248dcb5e9f1c7ad50402cc8b46e89b7
Sonja <sonjaljatkovic@gmail.com>
2023-11-08 12:52:27 -0500

    crerate a category code

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -29,12 +29,18 @@ router.get('/:id', async (req, res) => {
   }
 });
 
-router.post('/', (req, res) => {
-  // create a new category
+router.post('/', async (req, res) => {
+  try {
+    const newCategory = await Category.create(req.body);
+    res.status(200).json(newCategory);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a category by its `id` value
+  
 });
 
 router.delete('/:id', (req, res) => {
