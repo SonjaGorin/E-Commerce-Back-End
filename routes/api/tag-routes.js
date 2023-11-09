dd4817f0a9f866daa1d1b2f139b058ac972da689
Sonja <sonjaljatkovic@gmail.com>
2023-11-09 13:55:13 -0500

    fixed bugs with get routs

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -6,7 +6,7 @@ const { Tag, Product, ProductTag } = require('../../models');
 router.get('/', async (req, res) => {
   try {
     const tagData = await Tag.findAll({
-      include: [{ model: ProductTag }],
+        include: [{ model: Product }],
     });
     res.status(200).json(tagData);
   } catch (err) {
@@ -16,8 +16,8 @@ router.get('/', async (req, res) => {
 
 router.get('/:id', async (req, res) => {
   try {
-    const tagData = await Category.Tag(req.params.id, {
-      include: [{ model: ProductTag }]
+    const tagData = await Tag.findByPk(req.params.id, {
+      include: [{ model: Product }]
     });
     if (!tagData) {
       res.status(404).json({ message: "No tag found with that id!" });
